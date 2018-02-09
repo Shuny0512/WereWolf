c64d50c24d87677d493601f3bcdfd34250c94163
Shuny0512 <shun_ny@icloud.com>
2018-02-09 17:23:34 +0900

    プレイヤー人数操作関連のUIの設定

--- One Night Werewolf/Assets/Gamesetting/scripts/PlayerNumController.cs
+++ One Night Werewolf/Assets/Gamesetting/scripts/PlayerNumController.cs
@@ -3,22 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Playersettingcontroller : MonoBehaviour {
+public class PlayerNumController : MonoBehaviour {
 
 	GameObject player;
 	public int players;
-
+	public Button plus;
+	public Button minus;
 
 	void Start () {
-		this.player = GameObject.Find ("Howmanyplayers");
+		this.player = GameObject.Find ("howmanyplayers");
 		players = 4;
 		this.player.GetComponent<Text> ().text =
 			this.players.ToString ("F0");
+
+		plus.onClick.AddListener (Add);
+		minus.onClick.AddListener (Substruct);
 	}
 
 
 	void Update () {
 		
+	}
+
+	void Add () {
+		players++;
+	}
+
+	void Substruct () {
+		players--;
 	}
 		
 
