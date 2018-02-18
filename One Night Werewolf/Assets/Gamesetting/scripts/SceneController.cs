504f907fb75d76e0237f0046d6f6ed70aa92d669
Shuny0512 <shun_ny@icloud.com>
2018-02-18 15:30:58 +0900

    UI切り替え回数の制限設定

---
+++ One Night Werewolf/Assets/Gamesetting/scripts/SceneController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour 
+{
+	public Button TitleButton;
+	public Button StartButton;
+
+	void Start () 
+	{
+		TitleButton.onClick.AddListener (LoadTitle);
+		StartButton.onClick.AddListener(LoadNightAction);
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+
+	void LoadTitle ()
+	{
+		SceneManager.LoadScene ("Title");
+	}
+
+	void LoadNightAction()
+	{
+		SceneManager.LoadScene ("NightAction");
+	}
+}
--- One Night Werewolf/Assets/NightAction/Scripts/SwitchController.cs
+++ One Night Werewolf/Assets/NightAction/Scripts/SwitchController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SwitchController : MonoBehaviour 
 {
@@ -10,14 +11,17 @@ public class SwitchController : MonoBehaviour
 	public GameObject ResultUI;
 	public Button OpenButton;
 	public Button CompleteButton;
+	public int counter = 0;
+	//public int counterMax = 5;
+
 
 	void Start () 
 	{
+		
 		OpenUI.SetActive (true);
 		ResultUI.SetActive (false);
 		OpenButton.onClick.AddListener (Switch);
 		CompleteButton.onClick.AddListener (SwitchBack);
-
 		
 	}
 	
@@ -34,8 +38,26 @@ public class SwitchController : MonoBehaviour
 
 	void SwitchBack ()
 	{
-		OpenUI.SetActive (true);
-		ResultUI.SetActive (false);
+		counter++;
+
+		if (counter < 3) 
+		{
+			OpenUI.SetActive (true);
+			ResultUI.SetActive (false);
+			Debug.Log (this.counter);
+
+		} else 
+			
+		{
+			Debug.Log ("finish");
+			SceneManager.LoadScene ("DiscussionTime");
+
+		}
+			
+			
+
+
+
 	}
 
 
