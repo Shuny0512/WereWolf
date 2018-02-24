2f0c08ff6aef80d45ae1753a1c43d643a949fc20
Shuny0512 <shun_ny@icloud.com>
2018-02-24 18:45:54 +0900

    会議画面のUI部品作成

---
+++ One Night Werewolf/Assets/DiscussionTime/Scripts/StartDiscussionButtonController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class StartDiscussionButtonController : MonoBehaviour 
+{
+	public  Button DiscussionStartButton;
+
+	void Start () 
+	{
+		DiscussionStartButton.onClick.AddListener (LoadScene);
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+	void LoadScene ()
+	{
+		SceneManager.LoadScene ("Discussing");
+	}
+}
