dca1c61fcf25fc1f450822ec36ab87687d3f03db
Shuny0512 <shun_ny@icloud.com>
2018-03-04 14:29:56 +0900

    会議終了ボタンの作成

---
+++ One Night Werewolf/Assets/DiscussionTime/Scripts/DiscussingButtonController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DiscussingButtonController : MonoBehaviour {
+
+	public Button FinishdiscussionButton;
+
+	void Start () 
+	{
+		FinishdiscussionButton.onClick.AddListener (LoadScene);
+	}
+
+
+	void Update () {
+		
+	}
+
+	void LoadScene ()
+	{
+		SceneManager.LoadScene ("Executing");
+	}
+}
--- One Night Werewolf/Assets/DiscussionTime/Scripts/LeftsecondController.cs
+++ One Night Werewolf/Assets/DiscussionTime/Scripts/LeftsecondController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LectsecondController : MonoBehaviour 
+public class LeftsecondController : MonoBehaviour 
 {
 
 	public float leftsecond;
