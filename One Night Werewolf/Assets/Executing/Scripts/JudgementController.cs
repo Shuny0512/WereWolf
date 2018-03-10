fe68993427b0732e8b7685a2548fb90939f8807d
Shuny0512 <shun_ny@icloud.com>
2018-03-10 20:01:55 +0900

    Excutionシーン制作着手

---
+++ One Night Werewolf/Assets/Executing/Scripts/JudgementController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class JudgementController : MonoBehaviour {
+
+	public GameObject VotingUI;
+	public GameObject JudgementUI;
+	public Button JudgementButton;
+
+	void Start () {
+		
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
---
+++ One Night Werewolf/Assets/Result/Scripts/TitleButtonController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleButtonController : MonoBehaviour {
+
+	public Button BacktoTitleButton;
+
+	void Start () 
+	{
+		BacktoTitleButton.onClick.AddListener (LoadScene);
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+	void LoadScene () 
+	{
+		SceneManager.LoadScene ("Title");
+	}
+}
